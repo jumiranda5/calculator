9b23a87a3ef7e98b6c072fc129ab60f7df20320b
Juliana <jumiranda0501@gmail.com>
2022-12-19 09:54:49 -0300

    Add numbers buttons

--- script.js
+++ script.js
@@ -1,3 +1,42 @@
+// Vars
+let display = ""
+let cumulative = ""
+let total = 0
+
+// Elements
+const displayCumulative = document.querySelector('#cumulative')
+const displayTotal = document.querySelector('#total')
+const zero = document.querySelector('#zero')
+const one = document.querySelector('#one')
+const two = document.querySelector('#two')
+const three = document.querySelector('#three')
+const four = document.querySelector('#four')
+const five = document.querySelector('#five')
+const six = document.querySelector('#six')
+const seven = document.querySelector('#seven')
+const eight = document.querySelector('#eight')
+const nine = document.querySelector('#nine')
+
+zero.addEventListener('click', () => updateDisplay(0));
+one.addEventListener('click', () => updateDisplay(1));
+two.addEventListener('click', () => updateDisplay(2));
+three.addEventListener('click', () => updateDisplay(3));
+four.addEventListener('click', () => updateDisplay(4));
+five.addEventListener('click', () => updateDisplay(5));
+six.addEventListener('click', () => updateDisplay(6));
+seven.addEventListener('click', () => updateDisplay(7));
+eight.addEventListener('click', () => updateDisplay(8));
+nine.addEventListener('click', () => updateDisplay(9));
+
+
+// Update display
+function updateDisplay(n) {
+    display = display + n
+    displayTotal.textContent = display
+}
+
+
+// Function to perform operations
 function operate(a, b, operator, isPercentage) {
 
     let total = 0
@@ -90,22 +129,3 @@ const getPercent = (x) => {
         return getNumber(x)
     }
 }
-
-
-console.log("====== INTEGERS ======")
-console.log(`4 + 2 = ${operate("4", "2", "+", false)}`)
-console.log(`4 - 2 = ${operate("4", "2", "-", false)}`)
-console.log(`4 * 2 = ${operate("4", "2", "*", false)}`)
-console.log(`4 / 2 = ${operate("4", "2", "/", false)}`)
-
-console.log("====== FLOATS ======")
-console.log(`4.5 + 2.5 = ${operate("4.5", "2.5", "+", false)}`)
-console.log(`4.5 - 2.5 = ${operate("4.5", "2.5", "-", false)}`)
-console.log(`4.5 * 2.5 = ${operate("4.5", "2.5", "*", false)}`)
-console.log(`4.5 / 2.5 = ${operate("4.5", "2.5", "/", false)}`)
-
-console.log("====== PERCENTAGE ======")
-console.log(`500 + 10% = ${operate("500", "10%", "+", true)}`)
-console.log(`500 - 10% = ${operate("500", "10%", "-", true)}`)
-console.log(`500 * 10% = ${operate("500", "10%", "*", true)}`)
-console.log(`500 / 10% = ${operate("500", "10%", "/", true)}`)
